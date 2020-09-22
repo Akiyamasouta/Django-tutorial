005c44c8dba89268742c005e1de95342d4e400c8
Akiyamasouta <akiyama@DESKTOP-576N02Q.localdomain>
2020-09-22 21:18:29 +0900

    finish tutorial

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
 #from django.template import loader
-from .models import Question
+from .models import Question, Choice
+from django.views import generic
+from django.utils import timezone
 
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the polls index.")
@@ -29,8 +32,44 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question' : question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        #request.POST['choice']は選択された選択しのIDを文字列として返す
+        #POSTデータにchoiceがなければKeyErrorを送出
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': "You didn't select a choice"})
+
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        #postが成功したらHttpResponseRedirectを使う
+        #reverseを使うとURLのハードコードを防ぐ
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        return the last five published questions (not including those set to be
+        published in the future)
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        return Question.objects.order_by('-pub_date')[:5]
+
+    
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
